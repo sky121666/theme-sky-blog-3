bb5c225cd2559a42de41978f98b272aba9fa6274
sky <82988354+sky121666@users.noreply.github.com>
2026-07-21 16:44:47 +0800

    fix(Docsme): 修复项目卡片悬停渲染残影

--- templates/assets/js/chunks/widgets/system/clock/render.js
+++ templates/assets/js/chunks/widgets/system/clock/render.js
@@ -1,1 +1,1 @@
-import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=a44ffa1f7dcd";import{n as e}from"../../shared/clock-calendar.js?v=0.9.42&r=a44ffa1f7dcd";var d=r({renderWidget:()=>e});export{d as t};
+import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=cabe783533ba";import{n as e}from"../../shared/clock-calendar.js?v=0.9.42&r=cabe783533ba";var d=r({renderWidget:()=>e});export{d as t};
--- templates/assets/js/chunks/widgets/system/weather/render.js
+++ templates/assets/js/chunks/widgets/system/weather/render.js
@@ -1,1 +1,1 @@
-import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=a44ffa1f7dcd";import{t as e}from"../../shared/weather.js?v=0.9.42&r=a44ffa1f7dcd";var a=r({renderWidget:()=>e});export{a as t};
+import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=cabe783533ba";import{t as e}from"../../shared/weather.js?v=0.9.42&r=cabe783533ba";var a=r({renderWidget:()=>e});export{a as t};
