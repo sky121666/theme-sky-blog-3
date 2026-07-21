0733dc0c4ec2547c19d4382657a05af3ee596541
sky <82988354+sky121666@users.noreply.github.com>
2026-07-21 17:25:13 +0800

    fix(Finder): 适配分类与标签元数据图标颜色

--- templates/assets/js/chunks/widgets/system/clock/render.js
+++ templates/assets/js/chunks/widgets/system/clock/render.js
@@ -1,1 +1,1 @@
-import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=d93588f5ac98";import{n as e}from"../../shared/clock-calendar.js?v=0.9.42&r=d93588f5ac98";var d=r({renderWidget:()=>e});export{d as t};
+import{n as r}from"../../../rolldown-runtime.js?v=0.9.42&r=969957cacc92";import{n as e}from"../../shared/clock-calendar.js?v=0.9.42&r=969957cacc92";var d=r({renderWidget:()=>e});export{d as t};
